37c3f4bdfbaeabc69379980f760a5224d85a2a72
Saravana kumar G <71582563+saravana-quiz@users.noreply.github.com>
2021-07-06 01:09:31 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,17 @@
 ﻿using System;
 
-namespace Parameterizedoutput
+namespace MyFirstProgram
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int len = 0;
-            Console.WriteLine("Enter the String");
-            string str = Console.ReadLine();
-            foreach (char chr in str)
-            {
-                len += 1;
-            }
-            Console.WriteLine("length of the entered string is:{0}", len);
+            string firstName, lastName;
+            Console.WriteLine("enter your first name: ");
+            firstName = Console.ReadLine();
+            Console.WriteLine("enter your last name: ");
+            lastName = Console.ReadLine();
+            Console.WriteLine("Full Name:" + firstName + " " + lastName);
         }
     }
 }
