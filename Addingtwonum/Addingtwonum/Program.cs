91beaf7f4060a1ca426ad4531b4d0aca170e234e
Saravana kumar G <mailtoosaravana@gmail.com>
2021-07-06 00:53:09 +0530

    Assignment practice

---
+++ Addingtwonum/Addingtwonum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Adding_2_Num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 8, B = 80, Sum;
+            Sum = A + B;
+            Console.Write("Sum of two Num is:" + Sum);
+
+        }
+    }
+}
---
+++ Dividingtwonum/Dividingtwonum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dividingtwonum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A=80, B=10, result;
+            result = A / B;
+            Console.WriteLine("Result of Division of Two Numbers is:" + result);
+            
+        }
+    }
+}
