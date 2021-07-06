ea520c1b792f00f62011eb15509717c642c12e8f
Saravana kumar G <71582563+saravana-quiz@users.noreply.github.com>
2021-07-06 08:43:03 +0530

    Add files via upload

---
+++ Gettinginput-from-user/program1/Gettinginput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            string firstName, lastName;
+            Console.WriteLine("enter your first name: ");
+            firstName = Console.ReadLine();
+            Console.WriteLine("enter your last name: ");
+            lastName = Console.ReadLine();
+            Console.WriteLine("Full Name:" + firstName + " " + lastName);
+        }
+    }
+}
