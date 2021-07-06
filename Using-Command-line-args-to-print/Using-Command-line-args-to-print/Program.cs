be808d25a0c1f36a64caa595819d9e5856652fbd
Saravana kumar G <71582563+saravana-quiz@users.noreply.github.com>
2021-07-06 08:41:01 +0530

    Add files via upload

---
+++ Using-Command-line-args-to-print/Using-Command-line-args-to-print/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Using_Command_line_args_to_print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Name is " + args[0]);
+            Console.WriteLine("Last Name is " + args[1]);
+            Console.ReadLine();
+        }
+    }
+}
