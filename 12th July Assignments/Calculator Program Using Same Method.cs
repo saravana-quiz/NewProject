8a20e68c169f7dff618a1e758972a062a1942ad5
Saravana kumar G <71582563+saravana-quiz@users.noreply.github.com>
2021-07-13 01:22:46 +0530

    Assignments

---
+++ 12th July Assignments/Calculator Program Using Same Method.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculator_Program_Using_Same_Method_Names
+{
+    class Program
+    {
+        public static int Calculation(string Choice,int firstValue,int secondValue)
+        {
+            if (Choice == "Subtraction")
+            {
+                int result = firstValue - secondValue;
+                return result;
+            }
+            else if (Choice == "Division")
+            {
+                int result = firstValue / secondValue;
+                return result;
+            }
+            else
+                return 0;
+        }
+        public static int Calculation(String Choice, params string[] List)
+        {
+            if (Choice == "Addition")
+            {
+                int result = 0;
+                foreach (var i in List)
+                {
+                    result += int.Parse(i);
+
+                }
+                return result;
+
+            }
+            else if (Choice == "Multiplication")
+            {
+                int result = 1;
+                foreach (var j in List)
+                {
+                    result *= Convert.ToInt32(j);
+                }
+                return result;
+
+            }
+            else
+                return 0;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Numbers for Addition :");
+            var input = Console.ReadLine();
+            string[] newList = input.Split(",");
+            string Choice = "Addition";
+            var result1 = Program.Calculation(Choice, newList);
+            Console.WriteLine("The Addtion Result = " + result1);
+
+            Console.WriteLine("Enter the Numbers for Multiplication :");
+            var input1 = Console.ReadLine();
+            string[] newList1 = input1.Split(",");
+            string Choice1 = "Multiplication";
+            var result12 = Program.Calculation(Choice1, newList1);
+            Console.WriteLine("The Multiplication Result = " + result12);
+
+
+            Console.WriteLine("Enter the First Value for Subtraction :");
+            var firstVal = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Second Value for Subtraction :");
+            var secondVal = int.Parse(Console.ReadLine());
+            string Choice2 = "Subtraction";
+            var result2 = Program.Calculation(Choice2, firstVal,secondVal);
+            Console.WriteLine("The Subtraction Result = " + result2);
+
+            Console.WriteLine("Enter the  First Value for Division :");
+            var firstVal1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Second Value for Division :");
+            var secondVal2 = int.Parse(Console.ReadLine());
+            string Choice3 = "Division";
+            var result3 = Program.Calculation(Choice3, firstVal1, secondVal2);
+            Console.WriteLine("The Subtraction Result = " + result3);
+        }
+    }
+}
---
+++ 12th July Assignments/Decimal to DMS.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Decimal_To_DMS
+{
+    class Program
+    {
+        public static int Hour(double degree)
+        {
+            return (int)Math.Floor(degree);
+
+        }
+        public static int Seconds(double degree)
+        {
+            double Seconds = Math.Round(60 * degree);
+            return (int)Math.Round(Seconds);
+
+        }
+
+        public static void Minutes(double degree)
+        {
+            double Hour = Program.Hour(degree);
+            double min = degree - Hour;
+            double minute = min * 60;
+            var minutes = (int)Math.Floor(minute);
+            double seconds = minute - minutes;
+            int Seconds = Program.Seconds(seconds);
+            Console.WriteLine($"The Decimal degree {degree} coversion to DMS are :");
+            Console.WriteLine("Hours :"+Hour);
+            Console.WriteLine("Minutes :"+minutes);
+            Console.WriteLine("Second :"+Seconds);
+
+        }
+        
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter the Decimal Degree value : ");
+            double degree = Convert.ToDouble(Console.ReadLine());
+            Program.Minutes(degree);
+        }
+
+    }
+}
