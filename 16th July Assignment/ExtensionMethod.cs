49b8296239a0d9a9c6ca31e01e0e14ca2ee3f2d1
Saravana kumar G <71582563+saravana-quiz@users.noreply.github.com>
2021-07-18 13:18:36 +0530

    16th July Assignment

---
+++ 16th July Assignment/ExtensionMethod.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+/*3.Extension method
+  Create a class called Insurance, Add some methods like Pay premium.  
+  Have few properties like name, email, Premium Due Date, aadhar card number. 
+  Before creating an object, validate all inputs using the c# regular expressions.
+  Create an extension method called GetDueDays which accepts the Insurance object  
+  and returns the number of days pending to pay premium or number of days exceeded.*/
+
+namespace Extension_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validation InuputValidation = new Validation();  
+
+            Console.WriteLine("Enter Name :");          
+            string name = Console.ReadLine();
+            InuputValidation.Name = name;
+            var Name = InuputValidation.Name;
+
+            Console.WriteLine("Enter Email Address :");          
+            string email = Console.ReadLine();
+            InuputValidation.Email = email;
+            var Email = InuputValidation.Email;
+
+            Console.WriteLine("Enter Aadhar Number :"); 
+            string aadharNumber = Console.ReadLine();
+            InuputValidation.AadharNumber = aadharNumber;
+            var AadharNumber = InuputValidation.AadharNumber;
+
+            Insurance InsuranceObj = new Insurance();  
+            InsuranceObj.PayPremium();
+            var _dueDate = InsuranceObj.GetDueDays();  
+            Console.WriteLine($"Insurance Plan :Premium\nName :{Name}\nEmail :{Email}\nAadharNumber :{AadharNumber}\n{_dueDate.Days} Days left for your Due Date");
+        }
+    }
+    public class Insurance
+    {
+        public string Name { get; set; }
+        public string email { get; set; }
+        public double AadharNumber { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public void PayPremium()
+        {
+            DateTime PayDate = DateTime.Now;
+            DueDate = PayDate.AddDays(12);
+        }
+    }
+    class Validation
+    {
+        private string _name;
+        private string _email;
+        private string _aadharNumber;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                while (true)                //Name Validation
+                {
+                    Regex Name = new Regex(@"^[A-Za-z]\w{3,20}$");
+                    Match match = Name.Match(value);
+                    if (match.Success)
+                    {
+                        this._name = value;
+                        break;
+                    }
+                    Console.WriteLine("Invalid Name");
+                    Console.WriteLine("Enter the valid Name");
+                    string name = Console.ReadLine();
+                    value = name;
+                }
+            }
+        }
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                while (true)                 //Email Validation
+                {
+                    Regex Email = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+                    Match match = Email.Match(value);
+                    if (match.Success)
+                    {
+                        this._email = value;
+                        break;
+                    }
+                    Console.WriteLine("Invalid Email");
+                    Console.WriteLine("Enter the valid Email");
+                    string email = Console.ReadLine();
+                    value = email;
+                }
+            }
+        }
+        public string AadharNumber     
+        {
+            get { return _aadharNumber; }
+            set
+            {
+                while (true)     //Aadhar Number Validation
+                {
+                    Regex Aadhar = new Regex(@"[^0]{1}\d{11}");
+                    Match match = Aadhar.Match(value);
+                    if (match.Success)
+                    {
+                        this._aadharNumber = value;
+                        break;
+                    }
+                    Console.WriteLine("Invalid Aadhar Number");
+                    Console.WriteLine("Enter the valid Aadhar Number");
+                    string aadharNumber = Console.ReadLine();
+                    value = aadharNumber;
+                }
+            }
+        }
+    }
+    public static class ExtentionMethod
+    {
+
+        /*Extension Method which accepts the Insurance object  
+        and returns the number of days pending to pay premium*/
+        public static TimeSpan GetDueDays(this Insurance IObj)
+        {
+            var dueDate = IObj.DueDate;
+            TimeSpan DaysLeft = dueDate - DateTime.Now;
+            return DaysLeft;
+        }
+    }
+}
---
+++ 16th July Assignment/MethodHiding.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+/*1. Method Hiding 
+ 
+Create a Base Class with a method with the name WriteNum and display an integer. 
+Create another virtual method WriteStr and display some string.
+Create a Derived class and create a hidden method by name WriteNum and override the method WriteStr. 
+Create Objects -
+ -objectA  of type base class 
+- objectB of type  base class but downcasted to the derived class 
+- objectC of type derived class 
+Display the methods from all 3 objects*/
+
+namespace Method_Hiding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Base Class Objects :");
+            User details = new User();
+            details.WriteStr();
+            details.WriteNum();
+
+            Console.WriteLine("Derived Class Objects :");
+            NewUser newDetails = new NewUser();
+            newDetails.WriteStr();
+            newDetails.WriteNum();
+
+            //odownCastedObject of type  base class but downcasted to the derived class
+            Console.WriteLine("Object of type Base class but Downcasted to the Derived class");
+            var downCastedObject = (User)details;
+            downCastedObject.WriteStr();
+            downCastedObject.WriteNum();
+        }
+    }
+    class User
+    {
+        public virtual void WriteStr()  //virtual keyword is used to create virtual methods
+        {
+            string Name = "BATMAN";
+            Console.WriteLine("Name :"+Name);
+        }
+        public void WriteNum()
+        {
+
+            int age = 16;
+            Console.WriteLine("Age :"+age);
+        }
+    }
+    class NewUser : User
+    {
+        public override void WriteStr()
+        {
+            string NewName = "RICHY";
+            Console.WriteLine("New User Name :"+NewName);
+        }
+        public new void WriteNum()
+        {
+            int newAge = 18;
+            Console.WriteLine("New User Age :"+newAge);
+        }
+    }
+}
