6cf3abb8f4f2e5efccf00a13c8a8eb60ab56d55c
Saravana kumar G <71582563+saravana-quiz@users.noreply.github.com>
2021-07-19 23:06:36 +0530

    Done Assignments

---
+++ 19th July Assignments/Exception Handling/Domain/Library.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace Domain
+{
+    public class Library
+    {
+        public static string Occurance(string text)
+        {
+            StringBuilder builder = new StringBuilder("This is my Sample text occurance");
+
+            if (builder.ToString().Contains(text))
+            {
+                return "'Subtext Found' in Text Occurance";
+            }
+            else
+            {
+                throw new TextNotFoundException("Sub Text Not Found");
+            }
+        }  
+    }
+
+    public class TextNotFoundException : Exception
+    {
+        public TextNotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
