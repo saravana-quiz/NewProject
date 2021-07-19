898bf38fe739bf1498fb53b2c386bb8847373d8a
Saravana kumar G <71582563+saravana-quiz@users.noreply.github.com>
2021-07-19 23:10:07 +0530

    Add files via upload

---
+++ 19th July Assignments/Exception Handling/Presentation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Domain;
+
+namespace Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string text = "Sample text";
+                var result = Library.Occurance(text);
+                Console.WriteLine($"{text} found in Occurance");
+            }
+            catch(TextNotFoundException g)
+            {
+                Console.WriteLine(g.Message);
+            }
+            Console.WriteLine("Enter Any Key to Exit");
+            Console.ReadLine();
+        }
+    }
+}
