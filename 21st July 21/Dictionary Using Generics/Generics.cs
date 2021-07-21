997d9239706fdd02128a0e05ffa7800f820fd85b
Saravana kumar G <71582563+saravana-quiz@users.noreply.github.com>
2021-07-21 21:38:00 +0530

    Done Assignments

---
+++ 21st July 21/Dictionary Using Generics/Generics.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_Using_Generics
+{
+    class Generics<T, T1>
+    {
+        Dictionary<T, T1> dict = new Dictionary<T, T1>();
+
+        public void Add(T key, T1 value) => dict.Add(key, value);
+        public void Remove(T key) => dict.Remove(key);
+        public void sort()
+        {
+            var list = dict.Keys.ToList();
+            list.Sort();
+            foreach (var key in list)
+            {
+                
+                Console.WriteLine($"ID :{key},Name : {dict[key]}\n");
+            }
+        }
+
+        public void sort1()
+        {
+            var list = dict.Keys.ToList();
+            list.Sort();
+            foreach (var key in list)
+            {
+                Console.WriteLine($"Name :{key},ID : {dict[key]}\n");
+            }
+        }
+    }
+}
+
---
+++ 21st July 21/Dictionary Using Generics/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+/*1.  Generics
+
+Create your own  Dictionary which takes key , value pairs of type Generics. 
+Support methods like  Add, Remove and Sort.  Try this out on various user defined types. */
+
+namespace Dictionary_Using_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generics<int, string> genericList = new Generics<int, string>();
+            genericList.Add(8, "Richy Reginald");
+            genericList.Add(4, "Roshan");
+            genericList.Add(1, "Arun");
+            genericList.Add(2, "Black");
+            genericList.Add(5, "Dravid");
+            genericList.Add(3, "Messi");
+            genericList.Add(7, "Ronaldo");
+            genericList.Add(6, "Sam");
+
+            genericList.Remove(7);
+
+            genericList.sort();
+
+            Generics<string, double> genericList1 = new Generics<string, double>();
+            genericList1.Add("Tom Holland", 8000);
+            genericList1.Add("Dwayne Johnson", 88000);
+            genericList1.Add("Rock", 20000);
+            genericList1.Add("Chris Hemsworth", 80000);
+            genericList1.Add("Johnny Depp", 12005);
+            genericList1.Add("Jarvis", 16652);
+            genericList1.Add("robert downey jr", 180000);
+            genericList1.Add("Chris Evans", 190000);
+
+            genericList1.sort1();
+        }
+    }
+}
